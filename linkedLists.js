d4f8a4c6e452c39c4ac8a741d5cb36a7a5403ffc
Cedric Lapi <lapicedric@gmail.com>
2024-03-02 10:54:13 +0100

    new commit

--- linkedLists.js
+++ linkedLists.js
@@ -1,7 +1,3 @@
-//Fronts
-
-//Add Front
-
 class Node{
     constructor(data){
         this.data = data;
@@ -11,38 +7,68 @@ class Node{
 
 class LinkedList{
     constructor(){
+        
         this.head = null;
     }
 
     addFront(val){
-
-        let new_node = new Node(val);
-
+        var new_code = new Node(val);
 
         if(!this.head){
-            this.head = new_node;
+
+            this.head = new_code;
             return this.head;
 
         } else{
 
-            new_node.next = this.head;
-            this.head = new_node;
+            new_code.next = this.head;
+            this.head = new_code;
             return this.head;
-
         }
 
         
+
+
     }
+
+
+    remove(val){
+        var current_node = this.head;
+        var previous_node = null;
+
+        if(!current_node){
+            return;
+
+        } else if(current_node.data === val ){
+
+            this.head = current_node.next;
+            return this.head;
+            
+        } else{
+
+            while(current_node.data !== val){
+            previous_node = current_node;
+            current_node = current_node.next;
+
+        }
+
+    }
+
+    
+    previous_node.next = current_node.next;
 }
+}
+
+
 
 var list = new LinkedList();
 
-list.addFront(18);
-list.addFront(5);
-list.addFront(73);
+
+
+list.addFront(6);
+list.addFront(7);
+list.addFront(8);
+
+list.remove(8);
 
 console.log(list);
-
-
-
-
