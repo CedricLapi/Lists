b877bb56ce6dea55b4f4bc10a61ad36c252aedf5
Cedric Lapi <lapicedric@gmail.com>
2024-03-02 12:28:35 +0100

    new commit

--- linkedLists.js
+++ linkedLists.js
@@ -1,84 +1,69 @@
-class Node{
-    constructor(data){
+
+
+class Node {
+    constructor(data) {
         this.data = data;
         this.next = null;
     }
 }
 
-class LinkedList{
-    constructor(){
-        
+class LinkedList {
+    constructor() {
         this.head = null;
     }
 
-    addFront(val){
-        var new_code = new Node(val);
+    addFront(val) {
+        var new_node = new Node(val);
 
-        if(!this.head){
-
-            this.head = new_code;
-            return this.head;
-
-        } else{
-
-            new_code.next = this.head;
-            this.head = new_code;
-            return this.head;
+        if (!this.head) {
+            this.head = new_node;
+        } else {
+            new_node.next = this.head;
+            this.head = new_node;
         }
 
-        
-
-
+        return this.head.data;
     }
 
-
-    remove(val){
+    remove(val) {
         var current_node = this.head;
         var previous_node = null;
 
-        if(!current_node){
-            return;
-
-        } 
         
-        if(current_node.data === val ){
 
+        if (current_node.data === val) {
             this.head = current_node.next;
-            
+            return current_node.data;
+        }
 
-            return this.head;
-            
-        } 
-        
-        
-        
-
-            while(current_node.data !== val){
+        while (current_node && current_node.data !== val) {
             previous_node = current_node;
             current_node = current_node.next;
         }
 
+        if (!current_node) {
+            return null; 
+        }
 
         
-
         previous_node.next = current_node.next;
+        return current_node.data;
+    }
 
-    
-
-    
+    value() {
+        if (!this.head) {
+            return null;
+        }
+        return this.head.data;
+    }
 }
-}
-
-
 
 var list = new LinkedList();
-
-
 
 list.addFront(6);
 list.addFront(7);
 list.addFront(8);
-
 list.remove(8);
 
-console.log(list);
+//console.log(list.remove(8)); 
+console.log(list.value()); 
